f066b7bab9dbfe826d1ea5f1f8e3c5f536224099
yoon <fana9088@naver.com>
2024-11-25 22:35:37 +0900

    ball 약간구현 (중복숫자도 두번 카운트됨)

--- src/main/java/numberbaseball/Application.java
+++ src/main/java/numberbaseball/Application.java
@@ -15,6 +15,7 @@ public class Application {
         numbers.add(3);
 
         int strike = 0;
+        int ball = 0;
 
         System.out.println("숫자 야구 게임을 시작합니다!");
         System.out.println("1부터 9까지의 서로 다른 숫자 3개를 맞춰보세요.");
@@ -34,21 +35,36 @@ public class Application {
                 if (Objects.equals(userInput.get(i), numbers.get(i))) {
                     strike++;
                 }
+                for (int j = 0; j < 3; j++) {
+                    if (!Objects.equals(userInput.get(i), numbers.get(i)) &&
+                            Objects.equals(userInput.get(i), numbers.get(j)))
+                        ball++;
+                }
             }
+            // 유저 인풋이 1 1 1 이면
+//            for (int i = 0; i < 3; i++) {
+//                for (int j = 0; j < 3; j++) {
+//                    if (!Objects.equals(userInput.get(i), numbers.get(i)) &&
+//                            Objects.equals(userInput.get(i), numbers.get(j)))
+//                        ball++;
+//                }
+//            }
+
 
             // TODO: strike 개수를 계산하세요
             // TODO: 결과를 출력하세요 (예: "1 스트라이크")
             // TODO: 3 스트라이크인 경우 게임을 끝내세요
 
-            System.out.println(strike + " 스트라이크");
-            if (strike == 3){
+            System.out.println(strike + " 스트라이크" + ball + " 볼");
+            if (strike == 3) {
                 break;
             }
             strike = 0;
+            ball = 0;
 
 
         }
         System.out.println("축하합니다! 정답을 맞추셨습니다.");
     }
-
 }
+
